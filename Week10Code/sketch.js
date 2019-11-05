9402b8fc7260769978562e43391997b069b491f3
sgiacobetti13 <54879575+sgiacobetti13@users.noreply.github.com>
2019-11-04 18:55:04 -0800

    Add Week10Code Files

---
+++ Week10Code/sketch.js
@@ -0,0 +1,25 @@
+function setup() 
+{
+  createCanvas(400, 400);
+}
+
+function draw() 
+{
+  background(220,12,191);
+  textSize(37);
+  text('Abstract Sam!',10,30,);
+  textSize(17);
+  text('By Sam Giacobetti!',10,50,);
+  
+  circle(210,120,100);
+  triangle(140, 285, 208, 160, 316, 285);
+  rect(180,280,20,80);
+  rect(250,280,20,80);
+  line(150,180,170,230);
+  line(270,140,250,210);
+  point(140,150,190);
+  
+  circle(190,100,10);
+  circle(220,100,10);
+  ellipse(210, 140, 50, 20);
+}
